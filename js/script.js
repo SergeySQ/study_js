2a7d662a1c7a18f9ee405c5ea144174e753e08fe
SergeySQ <sergey.sudarev2016@yandex.kz>
2020-08-07 17:43:03 +0600

    добавление нового функционнала: использование методов и свойств

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
-
-alert('yea');
-console.log('21312');
+let money = 3000, 
+    income = 'фриланс', 
+    addExpenses = 'Интернет, такси, коммуналка', 
+    deposit = 212.5, 
+    mission = 200000, 
+    period = 5,
+    budgetDay = money/30;
+    console.log(typeof(money) ,typeof(income) , typeof(deposit) );
+    console.log(addExpenses.length);
+    console.log('Период равен ' +  period + ' месяцам ' +' и ' + ' Цель заработать ' + mission + ' рублей' );
+    console.log(addExpenses.toLowerCase().split(','));
+    console.log(budgetDay);
+    
